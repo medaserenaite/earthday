f2aeab5b4adb2afc8688fae799d864d701415411
Meda Serenaite <meda.serenaite@designory.com>
2020-02-24 13:41:01 -0600

    changes for heroku

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const path = require('path');
+require('./models/Product')
 
 const app = express();
 
@@ -8,6 +10,18 @@ mongoose.Promise = global.Promise;
 mongoose.connect(`mongodb://localhost:27017/test`, {useNewUrlParser: true, useUnifiedTopology: true});
 // mongoose.connect("mongodb://localhost:27017/test", { useNewUrlParser: true, useUnifiedTopology: true });
 app.use(bodyParser.json());
+
+require('./routes/productRoutes')(app);
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, 'client/build')));
+
+
+  app.get('*', (req,res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
