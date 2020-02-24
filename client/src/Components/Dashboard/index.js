f0dab010ca92c5d466e88abe97315114948a23b5
Meda Serenaite <meda.serenaite@designory.com>
2020-02-24 16:58:09 -0600

    adds sample site sections

---
+++ client/src/Components/Dashboard/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Dashboard extends Component {
+
+  render() {
+    return (
+      <div className="Dashboard">
+
+     </div>
+    );
+  }
+}
+
+export default Dashboard;
