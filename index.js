1affe06c9a40786ddf4b24c8a321a35098210acf
Meda Serenaite <meda.serenaite@designory.com>
2020-02-24 16:46:50 -0600

    adds comments

--- index.js
+++ index.js
@@ -2,17 +2,19 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const path = require('path');
-require('./models/Product')
+// require('./models/Product')
+require('./routes/productRoutes')(app);
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+//connects to database - needs some tweaking
 mongoose.Promise = global.Promise;
 mongoose.connect(`mongodb://localhost:27017/test`, {useNewUrlParser: true, useUnifiedTopology: true});
-// mongoose.connect("mongodb://localhost:27017/test", { useNewUrlParser: true, useUnifiedTopology: true });
+
 app.use(bodyParser.json());
 
-require('./routes/productRoutes')(app);
-
+// redirects all the requests to our frontend application, unless we specify any route before this code
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, 'client/build')));
 
@@ -20,10 +22,9 @@ if (process.env.NODE_ENV === 'production') {
   app.get('*', (req,res) => {
       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   })
-
 }
 
-const PORT = process.env.PORT || 5000;
+//listens to port (backend)
 app.listen(PORT, () => {
   console.log(`app running on port ${PORT}`)
 });
