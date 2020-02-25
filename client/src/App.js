7ae7ed5f1352496b8f8a0e6c9e099f6eb029c3ef
Meda Serenaite <meda.serenaite@designory.com>
2020-02-25 09:51:03 -0600

    adds google sign in button

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-       hello world
+       <div class="g-signin2" data-onsuccess="onSignIn"></div>
      </div>
     );
   }
