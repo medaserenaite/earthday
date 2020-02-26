f33b3740f7ca1c690e994d10d5830e34ab9aa284
Meda Serenaite <meda.serenaite@designory.com>
2020-02-26 15:57:22 -0600

    play around

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,20 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-       hello world
+       <form action="/" method="post">
+      <div>
+        <label>Username:</label>
+        <input type="text" name="username" />
+        <br/>
+      </div>
+      <div>
+        <label>Password:</label>
+        <input type="password" name="password" />
+      </div>
+      <div>
+        <input type="submit" value="Submit" />
+      </div>
+    </form>
      </div>
     );
   }
--- index.js
+++ index.js
@@ -2,16 +2,73 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const path = require('path');
+const passport = require('passport');
+const app = express();
+
+app.use(passport.initialize());
+app.use(passport.session());
 // require('./models/Product')
+
+app.get('/success', (req, res) => res.send("Welcome "+req.query.username+"!!"));
+app.get('/error', (req, res) => res.send("error logging in"));
+
+
+passport.serializeUser(function(user, cb) {
+  cb(null, user.id);
+});
+
+passport.deserializeUser(function(id, cb) {
+  User.findById(id, function(err, user) {
+    cb(err, user);
+  });
+});
 
 const PORT = process.env.PORT || 5000;
 
-const app = express();
+
 // require('./routes/productRoutes')(app);
 
 //connects to database - needs some tweaking
-mongoose.Promise = global.Promise;
-mongoose.connect(`mongodb://localhost:27017/test`, {useNewUrlParser: true, useUnifiedTopology: true});
+// mongoose.Promise = global.Promise;
+// mongoose.connect(`mongodb://localhost:27017/test`, {useNewUrlParser: true, useUnifiedTopology: true});
+
+mongoose.connect('mongodb://localhost/MyDatabase');
+
+const Schema = mongoose.Schema;
+const UserDetail = new Schema({
+      username: String,
+      password: String
+    });
+const UserDetails = mongoose.model('userInfo', UserDetail, 'userInfo');
+
+const LocalStrategy = require('passport-local').Strategy;
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+      UserDetails.findOne({
+        username: username
+      }, function(err, user) {
+        if (err) {
+          return done(err);
+        }
+
+        if (!user) {
+          return done(null, false);
+        }
+
+        if (user.password != password) {
+          return done(null, false);
+        }
+        return done(null, user);
+      });
+  }
+));
+
+// app.post('/',
+//   passport.authenticate('local', { failureRedirect: '/error' }),
+//   function(req, res) {
+//     res.redirect('/success?username='+req.user.username);
+//   });
 
 app.use(bodyParser.json());
 
